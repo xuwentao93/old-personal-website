99d8e09fd9f30efd0ea663be18777fed575cf05f
xuwentao93 <a15157756529@gmail.com>
2020-02-02 20:27:15 +0800

    recently article api finished

--- app/pages/main/recently/index.jsx
+++ app/pages/main/recently/index.jsx
@@ -1,57 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import './index.less'
-
-const recentlyArticleList = [
-  {
-    to: 'first-test',
-    title: '前端入门教程',
-    text: 'this is a very very very very long text...',
-    type: '前端',
-    time: '2020-01-01'
-  },
-  {
-    to: 'second-test',
-    title: 'javascript 进阶教程',
-    text: 'this is a very very very very long text...',
-    type: '前端',
-    time: '2020-01-01'
-  },
-  {
-    to: 'third-test',
-    title: '最新 ES2020 语法!',
-    text: 'this is a very very very very very very very very long text...',
-    type: '前端',
-    time: '2020-01-01'
-  },
-  {
-    to: 'fourth-test',
-    title: '最新 ES2020 语法!123',
-    text: 'this is a very very very very long text...',
-    type: '生活',
-    time: '2020-01-01'
-  },
-  {
-    to: 'fifth-test',
-    title: '最新 ES2020 语法!333ff',
-    text: 'this is a very very very very long text...',
-    type: '日记',
-    time: '2020-01-01'
-  }
-]
+import { getArticleMsg } from '@/api/article'
 
 export default function Recently() {
+  const [articleList, setArticleList] = useState([])
+  useEffect(() => {
+    getArticleMsg({
+      type: 'all'
+    })
+      .then((res) => {
+        setArticleList(res.data.result)
+      })
+      .catch((err) => console.log('err comes from getArticleMsg api: ' + err))
+  }, [])
   return (
     <div className="recently">
       <h3 className="title">近期文章</h3>
       <ul className="article-list">
         {
-          recentlyArticleList.map((article) => (
+          articleList.map((article) => (
             <li className="recently-article-link" key={article.to}>
               <h3><Link to={article.to} className="recently-article-title">{ article.title }</Link></h3>
               <div className="recently-article-text">{ article.text }</div>
               <div className="recently-article-brief">
-                <span className="recently-article-type">{ article.type }</span>
+                <span className="recently-article-type">{ article.label }</span>
                 <span className="recently-article-time">{ article.time }</span>
               </div>
             </li>
