2f3895d71126f87a632feeefe1ff721bad58bc77
xuwentao93 <a15157756529@gmail.com>
2020-03-02 21:59:58 +0800

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
   "rules": {
     "linebreak-style": 'off', // 不同操作系统换行符问题.
     "comma-dangle": ["error", "never"], // 对象最后一个不要加逗号.
-    "semi": ["error", "never"], // 不要分号
     "react/jsx-filename-extension": 'off', // js中用jsx语法报错.
     "import/extensions": 'off', // import .jsx文件报错.
     "no-console": 'off',
