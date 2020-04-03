9869066955ec96b6958e3215a8b6442e562dda3a
xuwentao93 <a15157756529@gmail.com>
2020-04-03 23:23:13 +0800

    carousel pending...

--- app/components/carousel/index.jsx
+++ app/components/carousel/index.jsx
@@ -3,20 +3,21 @@ import React, { useState } from 'react';
 import './index.less';
 
 export default function Carousel(props) {
+  const { imgList } = props;
   const [index, setIndex] = useState(0);
   // eslint-disable-next-line no-unused-vars
   const [autoPlay, setAutoPlay] = useState(null);
-  const { imgList } = props;
   // eslint-disable-next-line no-unused-vars
   const [showImg, setShowImg] = useState(Array(imgList.length).fill(-1));
   showImg[0] = 99;
   const methods = {
     next() {
-      console.log(index);
+      clearInterval(autoPlay);
+      setAutoPlay(null);
       if (index === imgList.length - 1) {
         setIndex(0);
-        showImg[showImg.length - 1] = -1;
         showImg[0] = 99;
+        showImg[imgList.length - 1] = -1;
       } else {
         showImg[index] = -1;
         showImg[index + 1] = 99;
@@ -24,6 +25,8 @@ export default function Carousel(props) {
       }
     },
     last() {
+      clearInterval(autoPlay);
+      setAutoPlay(null);
       if (index === 0) {
         setIndex(imgList.length - 1);
         showImg[0] = -1;
@@ -35,7 +38,34 @@ export default function Carousel(props) {
       }
     },
     autoPlay() {
-      setAutoPlay(setInterval(() => methods.next(), 2000));
+      setAutoPlay(setInterval(() => { // autoPlay 需要解决 setInterval 闭包引起的 state 无法实时更新.
+        let indexState = -1;
+        setIndex((currentIndex) => {
+          indexState = currentIndex;
+          return currentIndex;
+        });
+        if (indexState === imgList.length - 1) {
+          setIndex(0);
+          setShowImg((currentImg) => {
+            currentImg[imgList.length - 1] = -1;
+            currentImg[0] = 99;
+            return currentImg;
+          });
+        } else {
+          let currentImgNumber = NaN;
+          setIndex((currentIndex) => {
+            currentImgNumber = currentIndex + 1;
+            return currentIndex + 1;
+          });
+          console.log(currentImgNumber);
+          setShowImg((currentImg) => {
+            console.log(currentImg);
+            currentImg[currentImgNumber - 1] = -1;
+            currentImg[currentImgNumber] = 99;
+            return currentImg;
+          });
+        }
+      }, 2000));
     }
   };
   return (
